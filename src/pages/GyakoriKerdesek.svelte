6b23b59142ba39c00ee3c4d9fa8595eb9e6e2356
Varga-Tóth Máté <vargatothmate@gmail.com>
2026-04-16 14:01:41 +0200

    Add gyik questions

--- src/pages/GyakoriKerdesek.svelte
+++ src/pages/GyakoriKerdesek.svelte
@@ -14,4 +14,166 @@
   >
     GYAKORI KÉRDÉSEK
   </h1>
+
+  <div class="white-box">
+    <p class="question">
+      1. Kik vehetnek részt az „AHOL AZ ÍZ ÉS A JÓKEDV TALÁLKOZIK!” nyereményjátékban?
+    </p>
+    <p class="answer">
+      A Játékban kizárólag magyarországi lakó- vagy tartózkodási hellyel, valamint a magyar
+      hatóságok által kiállított érvényes személyazonosító igazolvánnyal rendelkező, 18. életévét
+      betöltött magyar állampolgárok vehetnek részt, akik a Játék időtartama alatt (2026. május 07.
+      - 2026. június 03.) a TESCO magyarországi üzleteiben vásárolnak legalább 1 db a promócióban
+      részt vevő tibi vagy Roshen márkájú terméket/termékeket, majd regisztrálnak a <a
+        class="underline"
+        href="https://www.tesco.tibi-roshen.hu"
+        target="_blank ">www.tesco.tibi-roshen.hu</a
+      > oldalon adataikkal (kötelezően megadandó: vezetéknév, keresztnév, e-mail cím, vásárlás dátuma,
+      a blokkon szereplő AP kód, a vásárlást igazoló blokkról készült fotó illetve scennelt kép feltöltésével,
+      nem kötelezően megadható: telefonszám) és beküldik azt.
+    </p>
+  </div>
+
+  <div class="white-box">
+    <p class="question">
+      2. Hogyan vehetek részt az „AHOL AZ ÍZ ÉS A JÓKEDV TALÁLKOZIK!” nyereményjátékban?
+    </p>
+    <p class="answer">
+      A játékban való részvételhez vásárolj legalább 1 db a promócióban részt vevő bármilyen tibi
+      vagy Roshen márkájú terméket/termékeket, majd az azt a vásárlást igazoló blokkról készült
+      képet a regisztráció során kért adatokkal kell beküldened. <br /><br />
+      Figyelem! A blokkot meg kell őrizni a játék lezárultát követő 30 napig!
+    </p>
+  </div>
+
+  <div class="white-box">
+    <p class="question">3. Számlával is részt vehetek a játékban?</p>
+    <p class="answer">
+      Sajnos nem, a játékban csak nyugtával (blokkal) vehetsz részt, melyen AP kód található. Céges
+      vásárlással (áfá-s számlával) nem vehetsz részt a játékban.
+    </p>
+  </div>
+
+  <div class="white-box">
+    <p class="question">4. Gyűjtőblokkal is részt vehetek a játékban?</p>
+    <p class="answer">
+      Sajnos nem, a játékban csak olyan nyugtával (blokkal) vehetsz részt, melyen a termékek
+      tételesen feltüntetésre kerülnek és egyértelműen látható és olvasható, hogy a
+      játékszabályzatban meghatározott termékek közül valamely termék vásárlása megtörtént.
+    </p>
+  </div>
+
+  <div class="white-box">
+    <p class="question">
+      5. Amennyiben több tibi vagy Roshen terméket vásárolok, viszont ez egy blokkon szerepel, akkor
+      többször is feltölthetem ugyanazt a blokkot külön pályázatként?
+    </p>
+    <p class="answer">
+      Sajnos nem.<br /><br />
+      Egy blokkal csak egyszer lehet játszani, így csak abban az esetben tudjuk külön pályázatként elfogadni,
+      ha azok külön blokkon szerepelnek és ha azokon a tibi vagy Roshen termék/termékek továbbra is szerepelnek.<br
+      /><br />
+      Azért, hogy a vásárlások megfelelően elkülönüljenek egymástól – azt javasoljuk, hogy vagy különböző
+      pénztárnál vásárolj (ekkor az AP kód változik), vagy a vásárlások között minimum 1 perc teljen el
+      (így a vásárlás időpontja fog eltérni)!
+    </p>
+  </div>
+
+  <div class="white-box">
+    <p class="question">6. Hol találom az AP kódot?</p>
+    <p class="answer">
+      Az AP kód, amely az AP betűket követően egy betűből és 8 számjegyből vagy 9 számjegyből áll
+      (pl: APA03100011 vagy AP123456789), minden egyes vásárlást igazoló tételes blokkon a legutolsó
+      sorban találod a vásárlás időpontjával (dátum, óra, perc) együtt.
+    </p>
+  </div>
+
+  <div class="white-box">
+    <p class="question">
+      7. Miket lehet nyerni az „AHOL AZ ÍZ ÉS A JÓKEDV TALÁLKOZIK!” nyereményjáték során?
+    </p>
+    <p class="answer">
+      Az „AHOL AZ ÍZ ÉS A JÓKEDV TALÁLKOZIK!” nyereményjáték során (2026. május 07. - 2026. június
+      03.) a következők kerülnek kisorsolásra: <br />
+
+      (naponta 2 db, összesen 56 db) Napi nyereményt; 10.000Ft értékű TESCO Ajándékutalvány<br />
+      (hetente 1 db, összesen 4 db) Heti nyereményt; Philips automata kávéfőző,<br />
+      valamint a játék végén 1db fődíj, összesen 500.000 Ft értékű TESCO Ajándékutalvány.
+    </p>
+  </div>
+
+  <div class="white-box">
+    <p class="question">
+      8. Hányszor nyerhetek az „AHOL AZ ÍZ ÉS A JÓKEDV TALÁLKOZIK!” nyereményjátékban?
+    </p>
+    <p class="answer">
+      A játék során akármennyi pályázatot beküldhetsz és minden beküldött pályázattal részt veszel
+      az aznapi és az a heti nyeremények sorsolásán, valamit a fődíj sorsolásán is, azonban a játék
+      teljes időtartama alatt legfeljebb 3x nyerhetsz napi nyereményt, 1x hetit és 1x fődíjat.
+    </p>
+  </div>
+
+  <div class="white-box">
+    <p class="question">9. Hogyan és mikor értesülök a nyertességről?</p>
+    <p class="answer">
+      A regisztráció során megadott e-mailcímre fogsz kapni egy értesítést arról, hogy nyertesnek
+      lettél jelölve.<br /><br /> A napi nyeremény esetében a beküldést követő egy munkanapon belül
+      kapsz értesítést, a heti nyeremény esetében pedig a következő hét első munkanapján kapsz
+      értesítést róla.<br /><br /> A fődíj esetében a játék lezáró napját követő 5 (öt) munkanapon
+      belül kapsz értesítést a nyertességről a regisztráció során megadott e-mail címre.<br /><br />
+      Az értesítésre minden esetben az értesítést követő, a jelen játékszabályzat szerinti 5 munkanapon
+      belül szükséges válaszüzenetben megadnod a kért adatokat, azaz a postázási címed, telefonszámod
+      (a csomag sikeres kézbesítése érdekében). <br /><br />Amennyiben a kért adatokat helyesen
+      adtad meg és időben beküldted, úgy nyertességed érvényessé válik. Az adatok sikeres
+      regisztrációját követően egy újabb e-mailben fogunk tájékoztatni.
+    </p>
+  </div>
+
+  <div class="white-box">
+    <p class="question">10. Meddig tart az „AHOL AZ ÍZ ÉS A JÓKEDV TALÁLKOZIK!” nyereményjáték?</p>
+    <p class="answer">
+      Az „AHOL AZ ÍZ ÉS A JÓKEDV TALÁLKOZIK!” nyereményjáték 2026. május 07. napján 00:01-től 2026.
+      június 03. napján 23:59-ig tart. <br /><br />
+      A Pályázatok beérkezésének határideje a Játék időtartamának vége, azaz 2026. június 03. 23 óra 59
+      perc 59 mp.
+    </p>
+  </div>
+
+  <div class="white-box">
+    <p class="question">11. Mikor kerülnek kiküldésre a nyeremények?</p>
+    <p class="answer">
+      Minden nyeremény a játék lezárulta után, az összes nyertesnek egyszerre kerül kiküldésre,
+      melyre várhatóan a Játék teljes időtartamának végétől számított 30 (harminc) naptári napon
+      belül kerül sor. <br /><br />
+      A fődíj esetében igény szerint személyes átadására a játék lezárta után, a nyertessel egyeztetett
+      időpontban kerül majd sor.
+    </p>
+  </div>
+
+  <div class="white-box">
+    <p class="question">
+      12. Mit tegyek, ha további kérdéseim lennének az „AHOL AZ ÍZ ÉS A JÓKEDV TALÁLKOZIK!”
+      nyereményjátékkal kapcsolatban?
+    </p>
+    <p class="answer">
+      További kérdések esetén olvasd el a részletes játékszabályzatot vagy küldj e-mail üzenetet az
+      info@tesco.tibi-roshen.hu e-mail címre.
+    </p>
+  </div>
 </section>
+
+<style>
+  @reference "tailwindcss";
+
+  .question {
+    @apply text-lg mb-4 font-bold;
+  }
+
+  .answer {
+    @apply text-base;
+  }
+
+  .white-box {
+    @apply max-w-[1240px] w-[95vw] bg-white rounded-3xl shadow-lg mx-auto mb-8 p-12 relative z-10;
+  }
+</style>
